850cbf50b1bbd492cd6f10da28d4baddfbdb1e28
Matty Williams <mattywilliams@google.com>
2019-11-21 00:52:54 +0000

    Tracking work on new iterative sample script

--- src/iterative_conditional_samples.py
+++ src/iterative_conditional_samples.py
@@ -8,32 +8,34 @@ import tensorflow as tf
 
 import model, sample, encoder
 
-def sample_model(
+def interact_model(
     model_name='1558M',
-    seed=1,
+    seed=None,
     nsamples=1,
     batch_size=1,
-    length=600,
+    length=None,
     temperature=1,
     top_k=0,
     top_p=1,
     models_dir='models',
+    input_file='input.txt',
+    output_file='output.txt',
+    iterations=3,
 ):
     """
-    Run the sample_model
+    Interactively run the model
     :model_name=1558M : String, which model to use
-    :seed=None : Integer seed for random number generators, fix seed to
-     reproduce results
-    :nsamples=0 : Number of samples to return, if 0, continues to
-     generate samples indefinately.
-    :batch_size=1 : Number of batches (only affects speed/memory).
+    :seed=None : Integer seed for random number generators, fix seed to reproduce
+     results
+    :nsamples=1 : Number of samples to return total
+    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
     :length=None : Number of tokens in generated text, if None (default), is
      determined by model hyperparameters
     :temperature=1 : Float value controlling randomness in boltzmann
      distribution. Lower temperature results in less random completions. As the
      temperature approaches zero, the model will become deterministic and
      repetitive. Higher temperature results in more random completions.
-    :top_k=500 : Integer value controlling diversity. 1 means only 1 word is
+    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
      considered for each step (token), resulting in deterministic completions,
      while 40 means 40 words are considered at each step. 0 (default) is a
      special setting meaning no restrictions. 40 generally is a good value.
@@ -41,40 +43,61 @@ def sample_model(
      (i.e. contains the <model_name> folder)
     """
     models_dir = os.path.expanduser(os.path.expandvars(models_dir))
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
+
     enc = encoder.get_encoder(model_name, models_dir)
     hparams = model.default_hparams()
     with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
         hparams.override_from_dict(json.load(f))
 
     if length is None:
-        length = hparams.n_ctx
+        length = hparams.n_ctx // 2
     elif length > hparams.n_ctx:
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
     with tf.compat.v1.Session(graph=tf.Graph()) as sess:
+        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
         tf.compat.v1.set_random_seed(seed)
-
         output = sample.sample_sequence(
             hparams=hparams, length=length,
-            start_token=enc.encoder['<|endoftext|>'],
+            context=context,
             batch_size=batch_size,
             temperature=temperature, top_k=top_k, top_p=top_p
-        )[:, 1:]
+        )
 
         saver = tf.compat.v1.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
 
-        generated = 0
-        while nsamples == 0 or generated < nsamples:
-            out = sess.run(output)
-            for i in range(batch_size):
-                generated += batch_size
-                text = enc.decode(out[i])
-                print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                print(text)
+        content = ""
+        for iteration in range(iterations):
+            if iteration > 0:
+                content += "\n\n"
+            raw_text = ""
+            with open(input_file, "r") as file:
+                raw_text = file.read()
+            context_tokens = enc.encode(raw_text)
+            generated = 0
+            for _ in range(nsamples // batch_size):
+                out = sess.run(output, feed_dict={
+                    context: [context_tokens for _ in range(batch_size)]
+                })[:, len(context_tokens):]
+                for i in range(batch_size):
+                    generated += 1
+                    text = enc.decode(out[i])
+                    content += "=" * 40 + " SAMPLE " + str(iteration + 1) + " " + "=" * 40
+                    content += "\n" + raw_text + text + "\n"
+            content += "=" * 80
+        
+        with open(output_file, "w") as file:
+            file.write(content)
+
+        print("All done!")
+        exit()
 
 if __name__ == '__main__':
-    fire.Fire(sample_model)
+    fire.Fire(interact_model)
 
